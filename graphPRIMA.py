17d489fd540b1aa2b3e51bca4171bc048aff6fa5
DiDaimon <35574670+DiDaimon@users.noreply.github.com>
2024-10-04 09:22:41 +0300

    мелкие правки

--- graphPRIMA.py
+++ graphPRIMA.py
@@ -1,48 +1,48 @@
 """Преобразует файл снятых характеристик(PRIMA) в html страницу
-
-версия 0_2:
-    - Олег добавил новую информационную строку в начало, пропуск строк изменен с 5 на 6
-Версия 0_3:
-    - Миграция на 4-тую версию Plotly
-    - Убрана функция получения имени файла (замена на метод .split())
-    - Добавлено чтение имен столбцов из файла
-    - Убрана тема по-умолчанию из графиков
-    - Изменен рендер по-умолчанию на браузер (pio.renderers)
-Версия 0_4:
-    - pio.renderers убран
-    - мелкие правки, улучшения кода(PEP8)
-    - добавлен вывод таблицей(полезная информация о графике)
-Версия 0_5:
-    - изменен принцип чтения колонок из файла и пропуск строк (теперь функция).
-    - фазовый шум читается правильно и весь.
-    - из КП теперь читаются все данные
-    - переработан принцип построения таблицы вспомогательных данных
-Версия 0_6:
-    - какого-то хрена на новом компе не открывается из plotly по .write_html(Auto_Open=True)
-Версия 0_7:
-    - добавлена пауза по окончанию выполнения программы, чтобы консоль с таблицей не исчезала
-    - после паузы удаляется файл html
-Версия 1_0:
-    - удалена функция create_columns_name ее функционал передан parsing_file.
-    - добавлена возможность чтения нескольких файлов.
-Версия 1_0_1
-    - добавлено циклический перебор цветов для мультиграфиков
-
-    TODO перейти вместо Plotly_html на Plotly Dash
-    TODO В конце концов решить проблему с шапкой файла с Олегом, стандартизировать шапку.
 """
-from art import tprint
-import plotly.graph_objs as go
-import pandas as pd
-import re
-import sys
-import tabulate
-import webbrowser
 import os
+import sys
+import webbrowser
 from itertools import cycle
 
+import magic
+import pandas as pd
+import plotly.graph_objs as go
+import tabulate
+from art import tprint
 
-def suffix_ru(number):
+
+def get_file_type(file):
+    """
+    Определяет тип файла по внутреннему содержимому.
+
+    Args:
+        file (str): Путь к файлу.
+
+    Returns:
+        str: Тип файла. Возможные значения: 'excel', 'text' или 'Unknown format: "{file_type}"'.
+    """
+    with open(file, 'rb') as f:
+        file_type = magic.from_buffer(f.read(), mime=False)
+    if 'excel' in file_type.lower():
+        return 'excel'
+    elif 'text' in file_type:
+        return 'text'
+    else:
+        return f'Unknown format: "{file_type}"'
+
+
+def suffix_ru(number: int) -> str:
+    """
+    Функция возвращает строку с суффиксом (файл, файла, файлов) в зависимости от
+    количества файлов
+
+    Args:
+        number (int): Количество файлов
+
+    Returns:
+        str: Суффикс
+    """
     if (number % 100) // 10 != 1 and number % 10 == 1:
         return 'файл'
     elif (number % 100) // 10 != 1 and number % 10 in [2, 3, 4]:
@@ -51,10 +51,26 @@ def suffix_ru(number):
         return 'файлов'
 
 
-def parsing_file(file):
+def excel_file_to_df(file):
     """
-    Функция читает переданный файл(с полным путем), преобразует данные из ПО PRIMA в Pandas DataFrame,
-    возвращает DataFrame.
+    Функция читает переданный файл(с полным путем), преобразует данные в формате Excel
+    из ПО PRIMA (множественные графики)в Pandas DataFrame, возвращает DataFrame.
+    Args:
+        file (str): Полный путь к файлу.
+
+    Returns:
+        (pd.DataFrame): DataFrame с характеристиками
+    """
+    df_temp = pd.read_excel(file)
+    df_temp = df_temp.drop(df_temp[df_temp[df_temp.columns[0]] == 0].index)
+    df_temp.rename(columns={df_temp.columns[0]: 'Freq'}, inplace=True)
+    return df_temp
+
+
+def txt_file_to_df(file):
+    """
+    Функция читает переданный файл(с полным путем), преобразует данные из ПО PRIMA в 
+    Pandas DataFrame, возвращает DataFrame.
     Args:
         file (str): Полный путь к файлу.
 
@@ -63,7 +79,7 @@ def parsing_file(file):
     """
 
     # Прочитаем файл и найдем строку со столбцами и выясним сколько строк надо пропустить
-    with open(file, 'r') as f:
+    with open(file, 'r', encoding='cp1251') as f:
         for idx, line in enumerate(f.readlines()):
             if 'Freq' in line:
                 columns_names = line
@@ -72,14 +88,40 @@ def parsing_file(file):
                 break
     # Форматируем строку и создаем список.
     columns_names = columns_names.replace("a (", "a(").split()
-    # Читаем файл пропустив первые skiprows строк и создаем DataFrame, с имена столбцов columns_names.
-    df_temp = pd.read_csv(file, sep='\t', skiprows=skiprows, names=columns_names, header=None, engine='python',
-                          encoding='cp1251')
+    # Читаем файл пропустив первые skiprows строк и создаем DataFrame,
+    #с имена столбцов columns_names.
+    df_temp = pd.read_csv(file, sep='\t', skiprows=skiprows, names=columns_names, header=None,
+                          engine='python', encoding='cp1251')
     print('[+] Парсинг успешен')
     return df_temp
 
 
-def add_table(df: pd.DataFrame, name: str):
+def parsing_file(file):
+    """
+    Функция парсит заданный файл и возвращает его содержимое в подходящем формате.
+    
+    Она определяет тип файла, вызывая функцию get_file_type, а затем делегирует 
+    парсинг либо excel_file_to_df, либо txt_file_to_df в зависимости от типа файла. 
+    Если тип файла не является ни 'excel', ни 'text', она выводит сообщение об ошибке 
+    и приостанавливает систему.
+    
+    Параметры:
+        file (str): Путь к файлу, который нужно обработать.
+    
+    Возвращает:
+        pd.DataFrame: Pandas DataFrame, содержащий содержимое файла.
+    """
+    if get_file_type(file) == 'excel':
+        return excel_file_to_df(file)
+    elif get_file_type(file) == 'text':
+        return txt_file_to_df(file)
+    else:
+        print('[-] Неправильный файл')
+        os.system('pause')
+        sys.exit()
+
+
+def add_table(df: pd.DataFrame):
     """
     Функция создает ДатаФрейм с исследовательскими данными.
 
@@ -91,8 +133,8 @@ def add_table(df: pd.DataFrame, name: str):
         (pd.DataFrame): ДатаФрейм с характеристиками
     """
     table = pd.DataFrame()
-    table['Наименование'] = ['Максимальное значение', 'Минимальное значение', 'Среднее значение', 'Медианное значение',
-                             'Стандартное отклонение']
+    table['Наименование'] = ['Максимальное значение', 'Минимальное значение', 'Среднее значение',
+                             'Медианное значение', 'Стандартное отклонение']
     for column in df.columns:
         if 'Freq' in column or 'Fmea' in column:
             continue
@@ -102,20 +144,27 @@ def add_table(df: pd.DataFrame, name: str):
 
 
 def unique_color():
+    """
+    Функция возвращает последовательность цветов для графиков.
+
+    Цвета берутся из стандартного набора matplotlib.
+
+    Returns:
+        cycle: последовательность цветов
+    """
     list_colors = cycle([
         'black', 'red', 'green', 'blue', 'purple', 'brown', 'magenta', 'orange', 'gray',
-        'coral',
-        'cadetblue', 'chocolate',  'cornflowerblue',
-        'crimson',  'firebrick', 'forestgreen', 'fuchsia', 'blueviolet',
-        'goldenrod',   'hotpink', 'indianred', 'indigo',   'maroon',
-        'navy', 'olive', 'olivedrab',  'orangered', 'orchid', 'palevioletred', 'peru',
-        'rosybrown', 'royalblue', 'rebeccapurple', 'saddlebrown', 'salmon',
-        'sandybrown', 'seagreen', 'slateblue', 'slategray', 'steelblue', 'tan', 'teal', 'tomato',
-        'turquoise', 'violet', 'dodgerblue', 'lightseagreen', 'lightslategray', 'limegreen', 'mediumblue',
-        'mediumorchid', 'mediumpurple', 'mediumseagreen', 'mediumslateblue', 'mediumturquoise',
-        'mediumvioletred', 'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgreen', 'darkmagenta',
-        'darkolivegreen', 'darkorange', 'darkorchid', 'darkred', 'darksalmon', 'darkseagreen',
-        'darkslateblue', 'darkslategray', 'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue'
+        'coral', 'cornflowerblue', 'crimson', 'firebrick', 'forestgreen', 'fuchsia',
+        'blueviolet', 'goldenrod', 'hotpink', 'indianred', 'indigo', 'maroon', 'navy',
+        'olive', 'olivedrab', 'orangered', 'orchid', 'palevioletred', 'peru', 'rosybrown',
+        'royalblue', 'saddlebrown', 'salmon', 'sandybrown', 'seagreen', 'slateblue',
+        'slategray', 'steelblue', 'tan', 'teal', 'tomato', 'turquoise', 'violet',
+        'dodgerblue', 'lightseagreen', 'lightslategray', 'limegreen', 'mediumblue',
+        'mediumorchid', 'mediumpurple', 'mediumseagreen', 'mediumslateblue',
+        'mediumturquoise', 'mediumvioletred', 'darkblue', 'darkcyan', 'darkgoldenrod',
+        'darkgreen', 'darkmagenta', 'darkolivegreen', 'darkorange', 'darkorchid',
+        'darkred', 'darksalmon', 'darkseagreen', 'darkslateblue', 'darkslategray',
+        'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue'
     ])
     return cycle(list_colors)
 
@@ -124,26 +173,23 @@ def add_graph(df, name):
     """
     Функция добавления графика из DataFrame.
     """
-    global graph
-    global color
-
     # добавляем график и его имя
     for column in df.columns:
         if 'Freq' in column or 'Fmea' in column:
             continue
-        graph.add_trace(
+        GRAPH.add_trace(
             go.Scatter(
                 x=df[df.columns[0]],
                 y=df[column],
                 name=f'{name} {column}',
                 line=dict(
-                    color=(next(color)),
+                    color=(next(COLOR)),
                     width=1
                 )
             )
         )
     # Далее основные настройки отображения
-    graph.update_layout(
+    GRAPH.update_layout(
         template="plotly_white",
         # title={
         #     'text': 'file_name',
@@ -172,7 +218,7 @@ def add_graph(df, name):
         )
     )
     # Коррекция всплывающей подсказки чтобы имя было целиком.
-    graph.update_layout(hoverlabel_namelength=-1)
+    GRAPH.update_layout(hoverlabel_namelength=-1)
 
     buttons = [
         dict(
@@ -186,7 +232,7 @@ def add_graph(df, name):
             args=[{'xaxis.type': 'log'}]
         ),
     ]
-    graph.update_layout(
+    GRAPH.update_layout(
         updatemenus=[
             dict(
                 type="buttons",
@@ -205,38 +251,49 @@ def add_graph(df, name):
 
 
 def main(files):
-    global graph
-    tprint('PRIMA - GRAPH', font='medium')
-    files_KP = []
-    files_KP.extend(files)
-    print(f'[+] Обнаружено: {len(files_KP)} {suffix_ru(len(files_KP))} данных')
+    """
+    Основная функция
+    """
+    tprint('graphPRIMA', font='medium')
+    upload_files = []
+    upload_files.extend(files)
+    print(f'[+] Обнаружено: {len(upload_files)} {suffix_ru(len(upload_files))} данных')
 
     graph_config = {
-        'toImageButtonOptions': {'format': 'png', 'filename': 'result', 'height': 1080, 'width': 1920, 'scale': 2}}
+        'toImageButtonOptions': {
+            'format': 'png', 
+            'filename': 'result', 
+            'height': 1080, 
+            'width': 1920, 
+            'scale': 2
+            }
+        }
 
-    for file_KP in files_KP:
-        filename = file_KP.split(sep="\\")[-1]
+    for file in upload_files:
+        filename = file.split(sep="\\")[-1]
         print(f'[+] Обработка файла: {filename}')
-        data_df = parsing_file(file_KP)
+        data_df = parsing_file(file)
         add_graph(data_df, filename[:-4])
-        print(f'[+] Таблица создана\n\n{filename}')
-        print(tabulate.tabulate(add_table(data_df, filename), headers=list(add_table(data_df, filename).columns),
-                                showindex=False, tablefmt="grid", stralign='left', numalign="left"), end='\n\n')
+        print(f'[+] Таблица создана\n\n Таблица файла: {filename}')
+        print(
+            tabulate.tabulate(
+                add_table(data_df),
+                headers=list(add_table(data_df).columns),
+                showindex=False, tablefmt="grid", stralign='left', numalign="left"
+            ),
+            end='\n\n'
+        )
 
-    graph.write_html('result' + ".html", auto_open=False, config=graph_config)
+    GRAPH.write_html('result' + ".html", auto_open=False, config=graph_config)
     print('[+] Файл с графиками создан')
     webbrowser.open('result' + ".html")
     os.system('pause')
     os.remove('result' + ".html")
 
 
+GRAPH = go.Figure()
+COLOR = unique_color()
+
 if __name__ == '__main__':
-    graph = go.Figure()
-    color = unique_color()
-    # KP = [
-    #     'C:\\Users\\truhachevda\\Dropbox\\Python\\АРК-НК4_№36АРК0-29-593_вибрация_20-400_кп.txt',
-    #     'C:\\Users\\truhachevda\\Dropbox\\Python\\АРК-НК4_№36АРК0-29-593_вибрация_20-400_кш.txt',
-    # ]
-    # KP = 'C:\\Users\\di_da\\Dropbox\\Python\\ПС9_№0003_вибрация_17,7-26,5.txt')  # Тестовый файл дома
-    KP = sys.argv[1:]
-    main(KP)
+    args = sys.argv[1:]
+    main(args)
